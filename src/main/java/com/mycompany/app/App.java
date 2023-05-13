2809da53f0cb63980127ed81dde67cba63d221fe
ragsmpl <rags.mpl@gmail.com>
2023-05-13 22:06:29 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,4 +16,22 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
+    
+
+    public String getMessage(String name) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (name == null || name.trim().length() == 0) {
+
+            result.append("Please provide a name!");
+
+        } else {
+
+            result.append("Hello " + name);
+
+        }
+        return result.toString();
+    }
+
 }
